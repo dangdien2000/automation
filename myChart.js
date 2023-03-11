ab71424e76057faa97d7b845af416bbe6b55cff5
dangdien2000 <127617003+dangdien2000@users.noreply.github.com>
2023-03-11 08:34:08 -0800

    Add files via upload

---
+++ myChart.js
@@ -0,0 +1,87 @@
+var ctx_live = document.getElementById("phcanvas");
+var myChart = new Chart(ctx_live, {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      data: [],
+      borderWidth: 1,
+      borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(153, 102, 255)',
+      'rgb(201, 203, 207)'
+    ],
+   
+      fill: false,
+      backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(201, 203, 207, 0.2)'
+    ],
+      label: 'pH',
+    }]
+  },
+  options: {
+    responsive: false,
+    title: {
+      display: true,
+      text: "",
+    },
+    legend: {
+      display: false
+    },
+
+    scales: {
+      x: {
+        display: true,
+        text: 'Value',
+        title: {
+          display: true,
+          text:'',
+          color: '#911',
+          font: {
+            family: 'Comic Sans MS',
+            size: 20,
+            weight: 'bold',
+            lineHeight: 1.2,
+          },
+          padding: {top: 0, left: 0, right: 0, bottom: 0}
+        }
+
+      },
+      y: {
+        display: true,
+        title: {
+          display: true,
+          text: 'Value',
+          color: '#191',
+          font: {
+            family: 'Times',
+            size: 20,
+            style: 'normal',
+            lineHeight: 1.2
+          },
+          padding: {top: 30, left: 0, right: 0, bottom: 0}
+        },
+        ticks: { min: 0 },
+      }
+    
+  },
+  }
+});
+setInterval(function() {
+  url= "IOServer.htm";
+  $.getJSON(url, function(data){
+    myChart.data.labels.push("");
+    myChart.data.datasets[0].data.push(data["pHChart"]);
+    myChart.update();
+  })
+},3000);
---
+++ myGauge.js
@@ -0,0 +1,243 @@
+
+setInterval(function() {
+url="IOServer.htm";
+$.getJSON(url, function(result){
+
+$('#Temp_Value').text(result["Temp_Value"]+ "");
+g1.refresh(result["Temp_Value"].trim());
+
+$('#Analog_Value').text(result["Analog_Value"]+ "");
+g2.refresh(result["Analog_Value"].trim());
+
+$('#Drive_Frequency').text(result["Drive_Frequency"]+ "");
+g3.refresh(result["Drive_Frequency"].trim());
+
+$('#Operation_Hour').text(result["Operation_Hour"]+ "");
+g4.refresh(result["Operation_Hour"].trim());
+
+$('#Water_Consumption').text(result["Water_Consumption"]+ "");
+g5.refresh(result["Water_Consumption"].trim());
+
+$('#pH_Value').text(result["pH_Value"]+ " ");
+g6.refresh(result["pH_Value"].trim());
+
+$('#Drive_Status').text(result["Drive_Status"]+ "");
+if(result["Drive_Status"]==0){
+      $('#Status_Run').text("Stop");
+      }
+    else{
+      $('#Status_Run').text("Running");
+      }
+
+
+//$('#meterChart').text(result["meterChart"]+ " ");
+});
+
+var d = new Date();
+  var n = d.toLocaleDateString();
+  var m = d.toLocaleTimeString();
+  $('#DateNowTime').text(m);
+  $('#DateNowDate').text(n);
+},1000);
+
+var g1 = new JustGage({
+  id: 'g1',
+  value: 0,
+  min: 25,
+  max: 55,
+  decimals: 3,
+  //showMinMax: false,
+  valueMinFontSize: 10,
+  label: "oC",	
+//levelColors: ["#00fff6","#ff00fc","#1200ff"],	
+  //textRenderer: customValue,
+  symbol: '',
+  pointer: true,
+  gaugeWidthScale: 0.6,
+  counter: true,
+  relativeGaugeSize: true,
+  //titleFontColor: "red",				
+  valueFontColor: "blue",
+  valueFontFamily: "Arial",
+  labelFontColor: "black",
+  //gaugeColor:	"red",
+  shadowSize: "5",
+  showInnerShadow: "false",
+  donut: false
+});
+function customValueg1(val) {				
+if (val < 4.5) {
+return 'v-low';
+      
+} else if (val <= 5.5) {
+return 'low';
+}
+else if (val <= 7.0) {
+return 'normal';
+}
+else if (val <= 7.5) {
+return 'high';
+}
+else {					
+return 'v-high';
+}
+}				
+
+var g2 = new JustGage({
+  id: 'g2',
+  value: 0,
+  min: 0,
+  max: 27648,
+  //symbol: '',
+  label:'0-27,648',
+  pointer: true,
+  pointerOptions: {
+    toplength: -15,
+    bottomlength: 10,
+    bottomwidth: 12,
+    color: '#000000',
+    stroke: '#ffffff',
+    stroke_width: 3,
+    stroke_linecap: 'round'
+  },
+  gaugeWidthScale: 0.3,
+  counter: true,
+  relativeGaugeSize: true,
+  donut: true,
+  valueFontColor: "blue",
+  valueFontFamily: "Arial",
+  labelFontColor: "black",
+});
+
+var g3 = new JustGage({
+  id: 'g3',
+  value: 0,
+  min: 0,
+  max: 6000,
+  symbol: '',
+  label: 'Hz',
+  pointer: true,
+  gaugeWidthScale: 0.6,
+  pointerOptions: {
+    toplength: 10,
+    bottomlength: 10,
+    bottomwidth: 8,
+    color: '#000'
+  },
+  counter: true,
+  relativeGaugeSize: true,
+  donut: false,
+  valueFontColor: "blue",
+  valueFontFamily: "Arial",
+  labelFontColor: "black",
+});
+
+var g4 = new JustGage({
+  id: 'g4',
+  value: 0,
+  min: 0,
+  max: 10000,
+  //symbol: ' Hrs',
+  label:'0-10,000 Hrs',
+  pointer: true,
+  
+  pointerOptions: {
+    toplength: 8,
+    bottomlength: -20,
+    bottomwidth: 6,
+    color: '#8e8e93'
+  },
+  gaugeWidthScale: 0.3,
+  counter: true,
+  relativeGaugeSize: true,
+  donut: true,
+  valueFontColor: "blue",
+  valueFontFamily: "Arial",
+  labelFontColor: "black",
+});
+
+var g5 = new JustGage({
+  id: 'g5',
+  value: 0,
+  min: 0,
+  max: 1000000,
+  symbol: '',
+  //label:'Gallon',
+  label:'0-1,000,000 Gal',
+  pointer: true,
+ 
+  pointerOptions: {
+    toplength: 8,
+    bottomlength: -20,
+    bottomwidth: 6,
+    color: '#8e8e93'
+  },
+  gaugeWidthScale: 0.3,
+  counter: true,
+  relativeGaugeSize: true,
+  donut: true,
+  valueFontColor: "blue",
+  valueFontFamily: "Arial",
+  labelFontColor: "black",
+});
+var g6 = new JustGage({
+  id: 'g6',
+  value: 0.0,
+  min: 2.0,
+  max: 8.0,
+  decimals: 3,
+  //symbol: '',
+  label:'2.00-8.00',
+  textRenderer: customValueg6,
+  pointer: true,
+  pointerOptions: {
+    toplength: 18,
+    bottomlength: 5,
+    bottomwidth: 6,
+    color: '#8e8e93'
+  },
+  gaugeWidthScale: 0.6,
+  counter: true,
+  relativeGaugeSize: false,
+  donut: false,
+  valueFontColor: "blue",
+  valueFontFamily: "Arial",
+  labelFontColor: "black",
+});
+  function customValueg6(val) {				
+  if (val < 3.50) {
+  return 'very-low';     
+  } 
+  if (val < 5.50) {
+  return 'low';     
+  } 
+  else if (val >= 5.51) {
+  return 'normal';
+  }
+  else if (val <= 7.20) {
+  return 'normal';
+  }
+  else if (val >= 7.21){					
+  return 'high';
+  }}				
+
+  setInterval(function() {
+  //g1.refresh(getRandomInt(0, 100));
+  //g2.refresh(getRandomInt(0, 27648));
+  //g3.refresh(getRandomInt(0, 6000));
+  //g4.refresh(getRandomInt(0, 10000));
+  //g5.refresh(getRandomInt(0, 1000000));
+  //g6.refresh(getRandomInt(2, 8));
+}, 5000); // update the charts every 5 seconds.
+
+  function fn_ScreenChange(scr_1, scr_2, scr_3, scr_4, scr_5, scr_6, scr_7){
+  document.getElementById(scr_1).style.visibility = 'visible';
+  document.getElementById(scr_2).style.visibility = 'hidden';
+  document.getElementById(scr_3).style.visibility = 'hidden';
+  document.getElementById(scr_4).style.visibility = 'hidden';
+  document.getElementById(scr_5).style.visibility = 'hidden';
+  document.getElementById(scr_6).style.visibility = 'hidden';
+  document.getElementById(scr_7).style.visibility = 'hidden';
+}
+
+
